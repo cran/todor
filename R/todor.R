bc35bb2f2a15f9ea081804d507e09d3cc15fbc8b
Dominik Krzemiński <raymon92@gmail.com>
2020-03-03 23:40:02 +0000

    version 0.1.0

--- R/todor.R
+++ R/todor.R
@@ -5,39 +5,57 @@
 #' @name todor
 NULL
 
-#' Default patterns
-DEFAULT_PATTERNS <- "default.csv"
-
 rex::register_shortcuts("todor")
 
 #' Todor addin
 #'
-#' Called on project that are not R packages. Checks all places in the code which require amendents
-#' as specified in \code{todo_types} on R and r files. When option \code{todor_rmd} is set to TRUE
-#' it searches also through Rmd files. Unless option \code{todor_exlude_packrat} is set to FALSE, all files in the packrat directory are excluded.
+#' Called on project that are not R packages. Checks all places in the code
+#' which require amendents as specified in \code{todo_types} on R and r files.
 #' It triggers rstudio markers to appear.
 #'
-#' @param todo_types vector with character describing types of elements to detect.
-#' If NULL default items will be used.
-#' @param search_path vector with paths that contains comments you are looking for.
-#' @param file character with path to file. If not NULL the search_path will be ignored.
+#' There are several options that let you control TODOr behaviour:
+#'
+#' \code{todor_rmd} - when set to TRUE it searches also through
+#' Rmd files (default TRUE).
+#'
+#' \code{todor_rnw} - when set to TRUE it searches also through
+#' Rnw files (default FALSE).
+#'
+#' \code{todor_rhtml} - when set to TRUE it searches also through
+#' Rhtml files (default FALSE).
+#'
+#' \code{todor_exlude_packrat} when set to FALSE, all files in the
+#' "packrat" directory are excluded (default TRUE).
+#'
+#' \code{todor_exclude_r} when TRUE, it ignores R and r files (default FALSE)
+#'
+#' \code{todor_patterns} must be vector. Contains all the names of patterns
+#' to be detected. Default are: "FIXME", "TODO", "CHANGED", "IDEA", "HACK",
+#' "NOTE", "REVIEW", "BUG", "QUESTION", "COMBAK", "TEMP".
+#'
+#' @param todo_types vector with character describing types of elements
+#' to detect. If NULL default items will be used.
+#' @param search_path vector with paths that contains comments you are
+#' looking for.
+#' @param file character with path to file. If not NULL the search_path
+#' will be ignored.
 #'
 #' @export
 #' @import rex
 #' @import utils
-#' @examples
-#' \donttest{
-#' todor()
-#' }
 todor <- function(todo_types = NULL, search_path = getwd(), file = NULL) {
   if (is.null(file)) {
-    files <- dir(
-      path = search_path,
-      pattern = rex::rex(".", one_of("Rr"), end),
-      recursive = TRUE,
-      full.names = TRUE
-    )
-    if (getOption("todor_rmd", FALSE)) {
+    if (getOption("todor_exclude_r", FALSE)) {
+      files <- c()
+    } else {
+      files <- dir(
+        path = search_path,
+        pattern = rex::rex(".", one_of("Rr"), end),
+        recursive = TRUE,
+        full.names = TRUE
+      )
+    }
+    if (getOption("todor_rmd", TRUE)) {
       rmdfiles <- list_files_with_extension("Rmd", search_path)
       files <- c(files, rmdfiles)
     }
@@ -49,7 +67,6 @@ todor <- function(todo_types = NULL, search_path = getwd(), file = NULL) {
       rhtmlfiles <- list_files_with_extension("Rhtml", search_path)
       files <- c(files, rhtmlfiles)
     }
-
     if (getOption("todor_exclude_packrat", TRUE)) {
       # Remove all filesnames, which include the packrat directory
       files <- files[!stringr::str_detect(files, "/packrat/")]
@@ -62,10 +79,9 @@ todor <- function(todo_types = NULL, search_path = getwd(), file = NULL) {
       files <- file
   }
   # Default TODO types
-  patterns <- as.character(read.csv(
-    system.file(DEFAULT_PATTERNS, package = "todor"),
-    stringsAsFactors = FALSE, header = FALSE)
-  )
+  patterns <- getOption("todor_patterns",
+                        c("FIXME", "TODO", "CHANGED", "IDEA", "HACK", "NOTE",
+                          "REVIEW", "BUG", "QUESTION", "COMBAK", "TEMP" ))
 
   if (is.null(todo_types))
     todo_types <- patterns
@@ -90,10 +106,6 @@ todor <- function(todo_types = NULL, search_path = getwd(), file = NULL) {
 #' If NULL default items will be used.
 #'
 #' @export
-#' @examples
-#' \donttest{
-#'  todor_package(c("FIXME"))
-#' }
 todor_package <- function(todo_types = NULL) {
   pkg_path    <- find_package()
   search_path <- file.path(pkg_path,
@@ -109,10 +121,6 @@ todor_package <- function(todo_types = NULL) {
 #' If NULL default items will be used.
 #'
 #' @export
-#' @examples
-#' \donttest{
-#'  todor_file("myfile.R")
-#' }
 todor_file <- function(file_name, todo_types = NULL) {
   todor(todo_types = todo_types, file = file_name)
 }
@@ -121,6 +129,7 @@ todor_file <- function(file_name, todo_types = NULL) {
 #'
 #' Calls \code{todor} function.
 #'
+#' @export
 #' @import rstudioapi
 todor_project_addin <- function() {
   project_path <- rstudioapi::getActiveProject()
@@ -133,6 +142,8 @@ todor_project_addin <- function() {
 #' Todor package addin
 #'
 #' Calls \code{todor_package} function.
+#'
+#' @export
 todor_package_addin <- function() {
   todor_package()
 }
